d803de705f04cdd3366717eea403f61a2df57ac9
RubanenkoV <58641047+RubanenkoV@users.noreply.github.com>
2020-10-11 18:36:06 +0300

    DZ 1 #1.7

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+/*Программа находит произведение двух чисел и выводит результат в консоль*/
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int month = in.nextInt();
+	    if (0 < month) && (month < )
+	    int day = in.nextLong();
+
+	    //System.out.println(a * b);
+	    //System.out.println("Рубаненко В.П. 16.09.2020, 14:56");
+		//System.out.println("task 1.7");
+
+
+		}
+}
